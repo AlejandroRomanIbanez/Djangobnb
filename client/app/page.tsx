0ef8bc261b1bad3e23b16a83658a1047836d656f
Alejandro Roman <romanibanez.alex@gmail.com>
2024-07-01 22:21:46 +0200

    Added navbar template

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Categories from "./components/Categories";
 
 export default function Home() {
   return (
-    <main className="">
-      Djangobnb
+    <main className="mx-auto px-6">
+      <Categories />
     </main>
   );
 }
